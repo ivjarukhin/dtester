1d2906850e6aed1a733108c632ea35a200a8f63e
unknown <jarukhin.io@gmail.com>
2016-07-01 00:16:40 +0400

    select item

--- controllers/universityCtrl.js
+++ controllers/universityCtrl.js
@@ -13,6 +13,45 @@ displayInfo.controller('displayInfoCtrl', ['$scope', '$http', function($scope, $
     	$http.get("http://ec2-52-38-234-109.us-west-2.compute.amazonaws.com//Group/getRecords")
             .then(function(response) {
                 $scope.resultGroup = response.data;
+                
+                console.log(event.target.innerText);
+                $scope.groupsSelected = [];
+                for (var i=0; i<$scope.result.length; i++) {
+                    if ($scope.result[i].faculty_name == event.target.innerText) {
+                        $scope.universitySelected = $scope.result[i].faculty_id;
+                        break;
+                    }
+                }
+
+                for (var j=0; j<$scope.resultGroup.length; j++) {
+                    if ($scope.resultGroup[j].faculty_id == $scope.universitySelected) {
+                        $scope.groupsSelected.push($scope.resultGroup[j]);
+                    }
+                }
+                console.log($scope.groupsSelected);
         });
-	};             
+	}; 
+    $scope.showStudents = function(event) {
+        $http.get("http://ec2-52-38-234-109.us-west-2.compute.amazonaws.com//Student/getRecords")
+            .then(function(response) {
+                $scope.resultStudents = response.data;
+                
+                console.log(event.target.innerText);
+                $scope.studentsSelected = [];
+                for (var i=0; i<$scope.resultGroup.length; i++) {
+                    if ($scope.resultGroup[i].group_name == event.target.innerText) {
+                        $scope.groupSelectedName = $scope.resultGroup[i].group_id;
+                        break;
+                    }
+                }
+
+                for (var j=0; j<$scope.resultStudents.length; j++) {
+                    if ($scope.resultStudents[j].group_id == $scope.groupSelectedName) {
+                        $scope.studentsSelected.push($scope.resultStudents[j]);
+                    }
+                }
+                console.log($scope.studentsSelected);
+                
+        });
+    };            
 }]);
